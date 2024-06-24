e1615326168f5fcf824006ec2c29bb5c6c3e05f3
陳泓達 <161418263+yhnjiuy4321@users.noreply.github.com>
2024-06-24 14:56:37 +0800

    upload on 20240624
    Improve comments.

--- Card_AddValue/CardMachine.java
+++ Card_AddValue/CardMachine.java
@@ -1,11 +1,12 @@
 public class CardMachine {
 
-    int getAmount(IcCard[] cards){     // cards = myCards;  // IcCard{card1,card2,card3}
-    //在getAmount方法裡放入陣列，陣列中的每個索引數都是「cards」
+    int getAmount(IcCard[] cards){
+    // cards = myCards;
+    // 已建立 ---> IcCard{card1,card2,card3}
+    //在getAmount方法裡放入IcCard類別的陣列，陣列中的每個索引數都是「cards」
 
         //建立容器
         int amount = 0;
-
 
         //建立for-each迴圈，把cards放入acard中，並加進容器(amount)中
         for (IcCard acard:cards) {
