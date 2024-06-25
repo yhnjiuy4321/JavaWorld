1cd823b753b0a95df7096acfc188af71aff69cbe
陳泓達 <161418263+yhnjiuy4321@users.noreply.github.com>
2024-06-25 16:16:30 +0800

    upload on 20240625

--- psw&id_Check/CheckId.java
+++ psw&id_Check/CheckId.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class CheckId {
     public static void main(String[] args) {
 
-        //使用者輸入學號 D1234567
+        //使用者輸入學號 ex:D1234567
         //檢查學號是否有效
 
         Scanner sc = new Scanner(System.in);
         System.out.print("請輸入學號: ");
         String word = sc.next();
 
-        if(word.length()==8) {
-            if(word.charAt(0)=='D') {//或用startsWith
-                if((word.substring(1,word.length()-1).matches("\\d{7}"))){
+        if(word.length()==8) { //長度為8碼
+            if(word.charAt(0)=='D') { //或用startsWith
+                if((word.substring(1,word.length()-1).matches("\\d{7}"))){ //後七碼必須是數字字串
                 System.out.println("該學號有效");
                 }
                 else {
@@ -32,30 +32,28 @@ public class CheckId {
 //""是物件
 //''是字元
 //第三步確認數字字串，也可以用迴圈+布林
+
 /*
 boolean isDigit=true;
-for(int i =1;i<word.length();i++)
-char ch = word.charAt(i);
-if(!()
-
+for(int i =1;i<word.length();i++){
+    char ch = word.charAt(i);
+    if(ch<'0'||ch>'9'){
+    isDigit=false;
+    break;
+    }
+}
 */
 
 /*
 
-if(word.matches("D[0-9]{7}")){} 直接一行表示
-
+if(word.matches("D[0-9]{7}")) 直接一行表示
 
 //?:0或一次
 //^=not
 //{x}出現x次
 //$:字串結尾
-//[0-9]=\\d
-//[0-9a-zA-Z_]=\\w
-
-System.out.println("該學號有效");
-
+//[0-9]=\\d(數字)
+//[0-9a-zA-Z]=\\w(任意英文與數字)
 }
-else{
-System.out.println("該學號錯誤");
-}
+
 */
--- psw&id_Check/id_email_pwse_Input.java
+++ psw&id_Check/id_email_pwse_Input.java
@@ -12,20 +12,19 @@ public class id_email_pwse_Input {
         String email = sc2.next();
 
         Scanner sc3 = new Scanner(System.in);
-        System.out.print("請輸入密碼: ");
+        System.out.print("請輸入密碼(長度8-10): ");
         String pswd = sc3.next();
 
 
         if (id.matches("[A-Z][1-2][0-9]{8}")) {
             if (email.matches("\\w+@(\\w+\\.)+\\w+")) {
-                    if (pswd.matches("\\w{8,10}") && pswd.matches(".*[a-zA-Z]+.*")&&pswd.matches(".*[\\d]+.*")) {
+                if (pswd.matches("\\w{8,10}") && pswd.matches(".*[a-zA-Z]+.*")&&pswd.matches(".*[\\d]+.*")) {
                        //長度8-10;要同時包含英文與數字
-                        //「.」任意字元;「*」出現零次以上;「+」至少一次;「\\w」任意英文與數字;「\\.」:點號
+                        //「.」任意字元;「*」出現零次以上;「+」至少一次;「\\w」任意英文與數字;「\\.」--->點號
 
                         System.out.println("格式皆正確");
-
                 }else {
-                System.out.println("密碼格式錯誤");
+                    System.out.println("密碼格式錯誤");
                 }
             } else {
                 System.out.println("信箱格式錯誤");
